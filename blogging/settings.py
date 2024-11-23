5ee9f78a1a8402d7638cb315dd8d678854cd7a65
charan <gr.charanreddy@gmail.com>
2024-11-23 23:44:04 +0530

    :art: password masked

--- blogging/settings.py
+++ blogging/settings.py
@@ -142,4 +142,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'iotstation2000@gmail.com'
-EMAIL_HOST_PASSWORD = 'ammanana@charan1'
+EMAIL_HOST_PASSWORD = '**************'
